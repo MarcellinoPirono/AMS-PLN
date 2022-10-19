391bf2f7d004137540e4a64b08bf20b8433a858a
fadhil27 <55023417+fadhil27@users.noreply.github.com>
2022-10-19 10:03:41 +0800

    main blade

--- laravel_ams/resources/views/layouts/main.blade.php
+++ laravel_ams/resources/views/layouts/main.blade.php
@@ -148,7 +148,7 @@
         ***********************************-->
         <div class="deznav">
             <div class="deznav-scroll">
-
+                <p></p>
                 <ul class="metismenu">
                     <li>
                         <a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
